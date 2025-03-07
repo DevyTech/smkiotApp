3910b49c4dd2a42c6cf832f1cca390204e19c813
DevyTech <bawotongdevaldy@gmail.com>
2025-03-07 20:31:22 +0800

    adwdw

--- app/src/main/java/com/example/smarthomeapp/MainActivity.java
+++ app/src/main/java/com/example/smarthomeapp/MainActivity.java
@@ -24,16 +24,14 @@ import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.materialswitch.MaterialSwitch;
 
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_status,tv_jendela;
-    private MaterialSwitch jendela;
-    private String url;
-    private RequestQueue requestQueue;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,61 +44,19 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        tv_status = findViewById(R.id.status);
-        tv_jendela = findViewById(R.id.jendelaStatus);
-        jendela = findViewById(R.id.servoJendela);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, new SensorFragment()).commit();
+        bottomNavigationView.setSelectedItemId(R.id.sensorMenu);
 
-        url = "http://192.168.1.54:8080";
-
-        requestQueue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
-
-        requestQueue.start();
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        int maxLength = Math.min(500, response.length());
-                        Log.d("Response","Response is : " + response.substring(0, maxLength));
-                        tv_status.setText("Online");
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Error to Connect"," : "+error);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.sensorMenu){
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, new SensorFragment()).commit();
+                return true;
+            } else if (itemId == R.id.servoMenu) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, new ServoFragment()).commit();
             }
+            return false;
         });
-
-        // Add a request to RequestQueue
-        MySingleton.getInstance(this).addToRequestQueue(stringRequest);
-
-
-
-        jendela.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                toggleJendela();
-                tv_jendela.setText(b ? "Terbuka" : "Tertutup");
-            }
-        });
-    }
-
-    private void toggleJendela(){
-        StringRequest request = new StringRequest(Request.Method.GET, url+"/servoJendela",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        int maxLength = Math.min(500, response.length());
-                        Log.d("Response Jendela","Response is : " + response.substring(0, maxLength));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Error to Connect"," : "+error);
-            }
-        });
-
-        // Add the request to the RequestQueue.
-        MySingleton.getInstance(this).addToRequestQueue(request);
     }
 }
--- app/src/main/java/com/example/smarthomeapp/ServoFragment.java
+++ app/src/main/java/com/example/smarthomeapp/ServoFragment.java
@@ -4,61 +4,91 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ServoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.material.materialswitch.MaterialSwitch;
+
 public class ServoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ServoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ServoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ServoFragment newInstance(String param1, String param2) {
-        ServoFragment fragment = new ServoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private TextView tv_status,tv_jendela;
+    private MaterialSwitch jendela;
+    private String url;
+    private RequestQueue requestQueue;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_servo, container, false);
+        View view = inflater.inflate(R.layout.fragment_servo, container, false);
+
+        tv_status = view.findViewById(R.id.status);
+        tv_jendela = view.findViewById(R.id.jendelaStatus);
+        jendela = view.findViewById(R.id.servoJendela);
+
+        url = "http://192.168.1.54:8080";
+
+        requestQueue = MySingleton.getInstance(getActivity()).getRequestQueue();
+
+        requestQueue.start();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        int maxLength = Math.min(500, response.length());
+                        Log.d("Response","Response is : " + response.substring(0, maxLength));
+                        tv_status.setText("Online");
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error to Connect"," : "+error);
+            }
+        });
+
+        // Add a request to RequestQueue
+        MySingleton.getInstance(getActivity()).addToRequestQueue(stringRequest);
+
+
+
+        jendela.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                toggleJendela();
+                tv_jendela.setText(b ? "Terbuka" : "Tertutup");
+            }
+        });
+
+
+        return view;
+    }
+
+    private void toggleJendela(){
+        StringRequest request = new StringRequest(Request.Method.GET, url+"/servoJendela",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        int maxLength = Math.min(500, response.length());
+                        Log.d("Response Jendela","Response is : " + response.substring(0, maxLength));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error to Connect"," : "+error);
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        MySingleton.getInstance(getActivity()).addToRequestQueue(request);
     }
 }
