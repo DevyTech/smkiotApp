9e5b9cbf8c5f2495bba823e13db306e0527e412f
DevyTech <bawotongdevaldy@gmail.com>
2025-03-27 21:50:39 +0800

    add splash screen & animate

---
+++ app/src/main/java/com/example/smarthomeapp/Splashscreen.java
@@ -0,0 +1,45 @@
+package com.example.smarthomeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splashscreen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView = findViewById(R.id.tv_splash);
+
+//        add animate fade in effect to the textview
+        textView.animate().alpha(1f).setDuration(7000);
+        textView.animate().scaleY(2f).scaleX(2f).setDuration(3000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
