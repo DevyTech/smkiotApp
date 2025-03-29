df4b398e6b220cbf58127b3abbb0489847b15d53
DevyTech <bawotongdevaldy@gmail.com>
2025-03-30 01:37:58 +0800

    change bottom navigation theme

--- app/src/main/java/com/example/smarthomeapp/ServoFragment.java
+++ app/src/main/java/com/example/smarthomeapp/ServoFragment.java
@@ -17,7 +17,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
-import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.materialswitch.MaterialSwitch;
 
 import org.json.JSONException;
