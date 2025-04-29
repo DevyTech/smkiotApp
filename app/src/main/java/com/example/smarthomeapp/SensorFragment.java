6cafe4c4a02a0ef7f0c7fb01911e870152a5a4e0
DevyTech <bawotongdevaldy@gmail.com>
2025-04-29 22:25:02 +0800

    awdawoijd

--- app/src/main/java/com/example/smarthomeapp/SensorFragment.java
+++ app/src/main/java/com/example/smarthomeapp/SensorFragment.java
@@ -4,61 +4,154 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SensorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+
 public class SensorFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Runnable sensorRunnable;
+    private Handler handler = new Handler();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SensorFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SensorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SensorFragment newInstance(String param1, String param2) {
-        SensorFragment fragment = new SensorFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    TextView suhu,kelembapan,air,jarak,gas,asap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sensor, container, false);
+        View view = inflater.inflate(R.layout.fragment_sensor, container, false);
+
+        suhu = view.findViewById(R.id.tvSuhu);
+        kelembapan = view.findViewById(R.id.tvKelembapan);
+        air = view.findViewById(R.id.tvAir);
+        jarak = view.findViewById(R.id.tvJarak);
+        gas = view.findViewById(R.id.tvGas);
+        asap = view.findViewById(R.id.tvAsap);
+
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startUpdater();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopUpdater();
+    }
+
+    private void startUpdater(){
+        sensorRunnable = new Runnable() {
+            @Override
+            public void run() {
+                getSuhu();
+                getJarak();
+                getGas();
+                getAsap();
+                handler.postDelayed(this, 500);
+                Toast.makeText(getActivity(), "Looper", Toast.LENGTH_SHORT).show();
+            }
+        };
+        handler.post(sensorRunnable);
+    }
+
+    private void stopUpdater(){
+        handler.removeCallbacks(sensorRunnable);
+        Toast.makeText(getActivity(), "Looper Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    private void getSuhu(){
+        JsonObjectRequest suhuRequest = new JsonObjectRequest(Request.Method.GET, MainActivity.mainActivity.url + "/kelembapan",
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String statusSuhu = response.getString("temperature");
+                    suhu.setText(statusSuhu +"\\u2103");
+                    String statusKelembapan = response.getString("humidity");
+                    kelembapan.setText(statusKelembapan +"\\u0025");
+                } catch (Exception e) {
+                    Log.e("Error get Jarak : ", e.toString());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error get Jarak : ", error.toString());
+            }
+        });
+        MySingleton.getInstance(getActivity()).addToRequestQueue(suhuRequest);
+    }
+
+    private void getJarak(){
+        JsonObjectRequest jarakRequest = new JsonObjectRequest(Request.Method.GET, MainActivity.mainActivity.url + "/jarak",
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String status = response.getString("distance");
+                    jarak.setText(status +" | CM");
+                } catch (Exception e) {
+                    Log.e("Error get Jarak : ", e.toString());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error get Jarak : ", error.toString());
+            }
+        });
+        MySingleton.getInstance(getActivity()).addToRequestQueue(jarakRequest);
+    }
+
+    private void getGas(){
+        StringRequest gasRequest = new StringRequest(Request.Method.GET, MainActivity.mainActivity.url + "/gas",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        gas.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error to get gas : ", error.toString());
+            }
+        });
+
+        MySingleton.getInstance(getActivity()).addToRequestQueue(gasRequest);
+    }
+
+    private void getAsap(){
+        StringRequest asapRequest = new StringRequest(Request.Method.GET, MainActivity.mainActivity.url + "/asap",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        asap.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error to get asap : ", error.toString());
+            }
+        });
+
+        MySingleton.getInstance(getActivity()).addToRequestQueue(asapRequest);
     }
 }
